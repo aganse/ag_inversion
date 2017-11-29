a49deb1c97d4f45b44e841d5cb6dea42030fa73b
Andy Ganse <andy@ganse.org>
2017-11-28 16:58:27 -0800

    got bayesian inv working with xforms

--- inv.py
+++ inv.py
@@ -7,36 +7,49 @@ import matplotlib.pyplot as plt
 from sklearn.base import BaseEstimator
 
 
-def convert_deriv_to_mag(J,m):
+def convert_deriv_to_mag(J,dpred):
     """
     Convert jacobian matrix of complex derivatives of predicted values wrt model params
     to a jacobian matrix of derivatives of the magnitudes of the predicted values wrt
     model parameters.
-    Requires inputs of both jacobian matrix J and model params m at which J is valid.
+    Requires inputs of both jacobian matrix J and predicted data dpred associated with J.
     (ie since typically this is used for a locally-linearized nonlinear model.)
     Assumes dims NxM for N predicted values and M model parameters
     """
     # Hm, this expression is quite off when comparing to findiffs, rederive:
     # Jmag = [np.conj(m) * J  +  m * np.conj(J)]  /2/np.abs(m)
-    Jmag = np.add( np.dot(np.conj(m.T),J) , np.dot(m.T,np.conj(J)) ) /2/np.abs(m)
+    Jmag = np.add( np.dot(np.conj(dpred.T),J) , np.dot(dpred.T,np.conj(J)) ) /2/np.abs(dpred)
     Jmag = np.real(Jmag)  # just stripping off the +0.j's and setting type to real
-    # That /2/np.abs(m) is a col vector of same length as m, and is supposed to
-    # divide element-wise into every column the matrix via Python's broadcasting.
+    # That /2/np.abs(dpred) divides element-wise into every column the matrix via Python's broadcasting.
     return Jmag
 
 
-def convert_deriv_to_log10(J,m):
+def convert_deriv_to_inv(J,dpred):
+    """
+    Convert jacobian matrix of complex derivatives of predicted values wrt model params
+    to a jacobian matrix of derivatives of the inverse of the predicted values wrt
+    model parameters.
+    Requires inputs of both jacobian matrix J and predicted data dpred associated with J.
+    (ie since typically this is used for a locally-linearized nonlinear model.)
+    Assumes dims NxM for N predicted values and M model parameters
+    """
+    Jinv = -J /dpred**2
+    # That /dpred**2 divides element-wise into every column the matrix via Python's broadcasting.
+    return Jinv
+
+
+def convert_deriv_to_log10(J,dpred):
     """
     Convert jacobian matrix of complex derivatives of predicted values wrt model params
     to a jacobian matrix of derivatives of the log10 of the predicted values wrt
     model parameters.
-    Requires inputs of both jacobian matrix J and model params m at which J is valid.
+    Requires inputs of both jacobian matrix J and predicted data dpred associated with J.
     (ie since typically this is used for a locally-linearized nonlinear model.)
     Assumes dims NxM for N predicted values and M model parameters.
-    Assumes m and J are already real-valued.
+    Assumes dpred and J are already real-valued.
     """
-    Jlog = np.log10(np.exp(1.0)) * J /m
-    # That /m divides element-wise into every column the matrix via Python's broadcasting.
+    Jlog = np.log10(np.exp(1.0)) * J /dpred
+    # That /dpred divides element-wise into every column the matrix via Python's broadcasting.
     return Jlog
 
 
@@ -343,19 +356,25 @@ class InvTB(BaseEstimator):
     demo_param : str, optional
         A parameter used for demonstation of how to pass and store paramters.
     """
-    def __init__(self, fwd_deriv_code, minit, mprior, Cprior, max_iter=5,
-                 dmtol=0.001, usefindiff=False, showplot=True, verbose=True):
+    def __init__(self, fwd_deriv_code, minit, mprior, Cprior, lb=-np.inf, ub=np.inf,
+                 max_iter=5, dmtol=1e-8, usefindiff=False, showplot=True, verbose=True):
+
+        if Cprior.ndim==1:
+            self.Cprior = np.diagflat(Cprior)
+        else:
+            self.Cprior = Cprior
         self.fwd_deriv_code = fwd_deriv_code
         self.minit = minit
         self.mprior = mprior
-        self.Cprior = Cprior
+        self.lb = lb
+        self.ub = ub
         self.max_iter = max_iter
         self.dmtol = dmtol
         self.usefindiff = usefindiff
         self.showplot = showplot
         self.verbose = verbose
 
-    def fit(self, ymeas, mmeas=None):
+    def fit(self, ymeas, ysigma, mmeas=None):
         """A reference implementation of a fitting function
         Parameters
         ----------
@@ -370,15 +389,20 @@ class InvTB(BaseEstimator):
             Returns self.
         """
 
-        Cinvchol = cholesky(inv(self.Cprior)).T
+        if np.isscalar(ysigma):
+            ysigma = ysigma*np.ones(len(ymeas))
+        elif len(ysigma)!=len(ymeas):
+            print('InvTB::fit() error:  len(ysigma)!=len(ymeas)')
+        ysigma = ysigma.reshape(len(ysigma),1)  # ensure it's a Nx1 ndarray not a 1d array
+        Dinvsqrt = np.diagflat(1.0/ysigma)  # (data inv covariance mtx)^(-1/2)
+        Cinvsqrt = cholesky(inv(self.Cprior)).T  # (model inv covariance mtx)^(-1/2)
 
-        # https://docs.scipy.org/doc/scipy/reference/optimize.html
-        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html
         def fun(m):
             mlen = m.size
             ypred,J = self.fwd_deriv_code(m)  # m: model params vector, J: derivs matrix
-            resids = ymeas-ypred
-            modelfunc = np.dot(Cinvchol,m) #np.subtract(m,np.squeeze(self.mprior)))
+            resids = ymeas-ypred  #ypred-ymeas
+            resids = np.dot(Dinvsqrt,resids)
+            modelfunc = np.dot(Cinvsqrt,np.subtract(m,np.squeeze(self.mprior)))
             modelfunc = modelfunc.reshape(len(modelfunc),1)
             f = np.squeeze(np.concatenate((resids,modelfunc),axis=0))
             return f
@@ -386,7 +410,8 @@ class InvTB(BaseEstimator):
         def jac(m):
             mlen = m.size
             ypred,J = self.fwd_deriv_code(m)  # m: model params vector, J: derivs matrix
-            Jreg = Cinvchol
+            J = np.dot(Dinvsqrt,J)
+            Jreg = Cinvsqrt
             Jout = np.concatenate((J,Jreg))
             return Jout
 
@@ -398,23 +423,25 @@ class InvTB(BaseEstimator):
             verblevel=2
         else:
             verblevel=0
+
         res = least_squares(fun, np.squeeze(self.minit), jac=jacfn,
-            bounds=(0., 3.5), diff_step=None, verbose=verblevel, max_nfev=self.max_iter,
+            bounds=(self.lb, self.ub), diff_step=1e-3, verbose=verblevel, max_nfev=self.max_iter,
             method='trf', ftol=1e-08, xtol=1e-08, gtol=1e-08, x_scale=1.0)
             #ftol=1e-4, xtol=1e-1, gtol=1e-8, x_scale=1.0)
             #ftol=1e0, xtol=1e-01, gtol=1e-01, x_scale=1.0)
             #ftol=1e-08, xtol=1e-08, gtol=1e-08, x_scale=1.0)
+        # https://docs.scipy.org/doc/scipy/reference/optimize.html
+        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html
 
         if mmeas is not None:
             testMSE = cplxMSE(res.x.reshape(len(res.x),1),mmeas)
         else:
             testMSE = npl.nan
         ypred,J = self.fwd_deriv_code(res.x.reshape(len(res.x),1))
-        ypred=np.log10(ypred)
         residnorm = norm(ypred-ymeas)
-        print('resid norm',residnorm)
+        #print('resid norm',residnorm)
         #print('maxeig JJ',np.real(np.amax(np.linalg.eigvals(np.dot(J.T,J)))))  # J'J has real eigvals but kept cplx type
-        #print('maxeig Cinv',np.amax(np.linalg.eigvals(np.dot(Cinvchol.T,Cinvchol))))
+        #print('maxeig Cinv',np.amax(np.linalg.eigvals(np.dot(Cinvsqrt.T,Cinvsqrt))))
         if self.showplot:
             f, ax = plt.subplots(1, 2, figsize=(11,4))
             # plot the meas and pred data:
@@ -425,15 +452,16 @@ class InvTB(BaseEstimator):
             ax[0].grid()
             #ax[0].set_ylabel('cost')
             #ax[0].set_xlabel('iterations')
-            ax[0].set_title('Measured (blk) and predicted (blu) data')
+            ax[0].set_title('Measured (blk) and predicted (red) data')
             # plot the init, true, and final model param vectors:
             ax[1].plot(self.minit,'g.-')
+            ax[1].plot(self.mprior,'c')
             ax[1].plot(res.x,'r.--')
             ax[1].plot(mmeas,'k.--')
             ax[1].grid()
             #ax[1].set_ylabel('model value')
             #ax[1].set_xlabel('indep var')
-            ax[1].set_title('Model vectors (true=blk, init=grn, soln=red)')
+            ax[1].set_title('Model vectors (meas=blk, pri=blu, ini=grn, sol=red)')
 
         # return m,cost,misfit,modelnorm,norm(dm),testMSE
         return res.x,res.cost,np.nan,np.nan,np.nan,testMSE
